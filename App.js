03d82402e49c0acc5c4fd2e9ec7c1ba5d4490fa8
carlosgodspeed <carlosminibics@gmail.com>
2024-05-18 13:19:19 -0300

    inicio

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ok funcionando</Text>
+      <Text>ok funcionando teste de atualização</Text>
+      <Text>teste de velocidade </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
