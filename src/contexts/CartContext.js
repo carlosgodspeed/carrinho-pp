3055329f463c9f61895c117f7f4c2e8ec00e4868
carlosgodspeed <carlosminibics@gmail.com>
2024-06-06 16:06:45 -0300

    adicionando produtos

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,10 +5,29 @@ export const CartContext = createContext({});
 function CartProvider({ children }){
   const [cart, setCart] = useState([]);
   
+  function addItemCart(newItem){
+    const indexItem = cart.findIndex(item => item.id === newItem.id)
+
+    if(indexItem !== -1){
+
+    }
+
+    let data = {
+      ...newItem,
+      amount:1,
+      total: newItem.price
+    }
+
+    setCart(products => [...products, data])
+    console.log([...cart,data])
+    
+  }
+
   return(
     <CartContext.Provider
       value={{
-        cart
+        cart,
+        addItemCart
       }}
     >
       {children}
