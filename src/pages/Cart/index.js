8c994c78a2b3e5a3b4d92b075c169cee65cf6af3
carlosgodspeed <carlosminibics@gmail.com>
2024-07-09 11:30:52 -0300

    listando compras

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,15 +1,30 @@
-import { View, Text, StyleSheet} from 'react-native'
+import { useContext } from 'react'
+import { View, Text, StyleSheet, FlatList} from 'react-native'
+import { CartContext } from '../../contexts/CartContext'
 
 export default function Cart(){
+  const { cart, addItemCart } = useContext(CartContext);
+
   return(
     <View style={stlyes.container}>
-      <Text>Pagina Carrinho</Text>
+      <FlatList
+        data={cart}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={ (item) => String(item.id) }
+        renderItem={ ({item}) => {
+
+        }}
+      />
     </View>
   )
 }
 
 const stlyes = StyleSheet.create({
   container:{
-    flex:1
+    flex:1,
+    backgroundColor:'#FAFAFA',
+    paddingStart:14,
+    paddingEnd:14,
+    paddingTop:14,
   }
 })
